c459001cc2e1972abbb47e5734f7ae8976efb718
juanmz1606 <juan.1701920466@ucaldas.edu.co>
2026-01-06 23:34:22 -0500

    Commit inicial

---
+++ src/data/projects.js
@@ -0,0 +1,8 @@
+export const projects = [
+  {
+    title: "Restaurant Management System",
+    description: "Full stack system with NestJS, MongoDB, React and Docker.",
+    tech: ["NestJS", "MongoDB", "React", "Docker"],
+    github: "https://github.com/juanmz1606/restaurant_project_docker.git",
+  },
+];
